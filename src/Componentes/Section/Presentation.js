9cd2c74eae6addc60c705bdfea71ca0ea1f53715
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2024-05-28 21:41:14 -0300

    useEffect

--- src/Componentes/Section/Presentation.js
+++ src/Componentes/Section/Presentation.js
@@ -1,11 +1,49 @@
 import styles from './Presentation.module.css';
 import ButtonA from '../Elements/ButtonA';
+import {useEffect, useState} from 'react'
 
 function Presentation(){
+
+    const [text, setText] = useState('')
+    const toRotate = ['Kevin Ruan!', 'Development Front End', 'Development Web'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+
+
+
+    useEffect(() =>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+
+    }
+
     return (
         <div className={styles.presentation} id="Presentation">
             <h4><strong>Bem-vindo ao meu Porfólio</strong></h4>
-            <h1>Olá, eu sou Kevin Ruan!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
             Cresci fascinado por telas e videogames, o que me levou<br/>
             a explorar o mundo da programação. Recentemente, iniciei<br/>
