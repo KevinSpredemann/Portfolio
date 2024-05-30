da24418ee025e4a2be1829b03c5ba942263f93b9
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2024-05-29 21:30:07 -0300

    Hover changes

--- src/Componentes/Elements/Cards.js
+++ src/Componentes/Elements/Cards.js
@@ -1,34 +1,20 @@
 import styles from './Cards.module.css';
 import ButtonB from './ButtonB';
-import {useState} from 'react'
 
 function Cards({img, title, tech, description, repo, site}){
 
-    const [info, setInfo] = useState(false)
-
-    function InfoOn(){
-        setInfo(true)
-    }
-
-    function InfoOff(){
-        setInfo(false)
-    }
-
-
     return (
-        <div onMouseOut={InfoOff} className={styles.cards}>
-            <a onMouseEnter={InfoOn} href={site}>
+        <div className={styles.cards}>
+            <a href={site}>
                 <img src={img} alt=""/>
             </a>
 
-            {info === true && (
                  <section>
                  <h3>{title}</h3>
                  <p><strong>Tecnologias: </strong>{tech}</p>
                  <p>{description}</p>
                  <ButtonB text='Acesse o meu repositório' link={repo}/>
-             </section>
-            )}      
+             </section>      
         </div>
     )
 }
